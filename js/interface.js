5df879fd9fcc8c94ab4774731158a7c1530bf621
Nicholas Valbusa <squallstar@gmail.com>
2016-09-27 16:42:55 +0200

    well. just a bit of refactor

--- js/interface.js
+++ js/interface.js
@@ -1,23 +1,28 @@
+(function () {
+
 var $folderList = $('#folder_list');
 var templates = {
   folder: template('folder'),
-  app: template('app')
+  app: template('app'),
+  organisation: template('organisation')
 };
 
 var data = Fliplet.Widget.getData();
 var widgetId = Fliplet.Widget.getDefaultId();
 
-function getFolder() {
+function initialise() {
   Fliplet.Media.Folders.get({
     appId: Fliplet.Env.get('appId')
   }).then(function (response) {
-    return Fliplet.Apps.get( Fliplet.Env.get('appId') ).then(function (apps) {
-      apps[0].name = "Root folder (" + apps[0].name + ")";
-      apps.forEach(addApp);
-      response.folders.forEach(addFolder);
-      return Promise.resolve();
-    });
-  }).then(initialiseData);
+    response.folders.forEach(addFolder);
+    return Fliplet.Apps.get();
+  }).then(function (apps) {
+    apps.forEach(addApp);
+    return Fliplet.Organizations.get();
+  }).then(function (organisations) {
+    organisations.forEach(addOrganisationFolder);
+    initialiseData();
+  });
 }
 
 // Adds app root folder template
@@ -30,55 +35,78 @@ function addFolder(folder) {
   $folderList.append(templates.folder(folder));
 }
 
+// Adds organisation item template
+function addOrganisationFolder(org) {
+  $folderList.append(templates.organisation(org));
+}
+
 // Templating
 function template(name) {
   return Handlebars.compile($('#template-' + name).html());
 }
 
-function save(notifyComplete) {
+function save() {
+  var $el = $folderList.find('option:selected');
+  var val = $el.val();
 
   data.appID = '';
   data.folderID = '';
+  data.organisationID = '';
 
-  if ( $('#folder_list option:selected').attr('data-app') !== undefined ) {
-    data.appID = $('#folder_list option:selected').val();
+  if ($el.data('app')) {
+    data.appID = val;
+  } else if ($el.data('organisation')) {
+    data.organisationID = val;
   } else {
-    data.folderID = $('#folder_list option:selected').val();
+    data.folderID = val;
   }
 
-  if(notifyComplete) {
-    Fliplet.Widget.save(data).then(function () {
-      Fliplet.Studio.emit('reload-page-preview');
-      Fliplet.Widget.complete();
-    });
-  }
+
+  Fliplet.Widget.save(data).then(function () {
+    Fliplet.Studio.emit('reload-page-preview');
+    Fliplet.Widget.complete();
+  });
 }
 
 Fliplet.Widget.onSaveRequest(function () {
-  save(true);
+  save();
 });
 
-$('#folder_list').on('change', function() {
-  var selectedValue = $(this).val();
-  var selectedText = $(this).find("option:selected").text();
-  $('.section.show').removeClass('show');
-  $('#' + selectedValue + 'Section').addClass('show');
-  $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
-});
+// Not sure what this crap is
+// $folderList.on('change', function() {
+//   var selectedValue = $(this).val();
+//   var selectedText = $(this).find("option:selected").text();
+//   $('.section.show').removeClass('show');
+//   $('#' + selectedValue + 'Section').addClass('show');
+//   $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
+// });
 
 $('#help_tip').on('click', function() {
   alert("During beta, please use live chat and let us know what you need help with.");
 });
 
 function initialiseData() {
-  if (data != undefined ) {
-    if (data.appID != '') {
-      $('#folder_list [data-app][value="'+data.appID+'"]').attr("selected","selected");
-    } else {
-      $('#folder_list [data-folder][value="'+data.appID+'"]').attr("selected","selected");
-    }
-    $('#folder_list').change();
+  if (!data) {
+    return;
   }
+
+  var $context;
+
+  if (data.appID) {
+    $context = $folderList.find('[data-app][value="'+data.appID+'"]');
+  } else if (data.organisationID) {
+    $context = $folderList.find('[data-organisation][value="'+data.organisationID+'"]');
+  } else {
+    $context = $folderList.find('[data-folder][value="'+data.folderID+'"]');
+  }
+
+  if ($context) {
+    $context.attr("selected","selected");
+  }
+
+  $folderList.change();
 }
 
-getFolder();
+initialise();
+
+})();
