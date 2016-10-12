535619827a06d92cbf5ea5e642111d3271f59888
Ricardo Seromenho <ricardo@seromenho.com>
2016-10-12 18:30:28 +0100

    enahncements
    Navigate through folders

--- js/interface.js
+++ js/interface.js
@@ -1,108 +1,224 @@
 (function () {
+  var data = Fliplet.Widget.getData();
+  var $imagesContainer = $('.image-library');
+  var templates = {
+    folder: template('folder'),
+    app: template('app'),
+    organization: template('organization'),
+    noFiles: template('nofiles')
+  };
 
-var $folderList = $('#folder_list');
-var templates = {
-  folder: template('folder'),
-  app: template('app'),
-  organisation: template('organisation')
-};
+  function addFolder(folder) {
+    $imagesContainer.append(templates.folder(folder));
+  }
 
-var data = Fliplet.Widget.getData();
-var widgetId = Fliplet.Widget.getDefaultId();
+  function addApp(app) {
+    $imagesContainer.append(templates.app(app));
+  }
 
-function initialise() {
-  Fliplet.Media.Folders.get({
-    appId: Fliplet.Env.get('appId')
-  }).then(function (response) {
-    response.folders.forEach(addFolder);
-    return Fliplet.Apps.get();
-  }).then(function (apps) {
-    apps.forEach(addApp);
-    return Fliplet.Organizations.get();
-  }).then(function (organisations) {
-    organisations.forEach(addOrganisationFolder);
-    initialiseData();
+  function addOrganization(organization) {
+    $imagesContainer.append(templates.organization(organization));
+  }
+
+  function noFiles() {
+    $imagesContainer.html(templates.noFiles());
+  }
+
+  function template(name) {
+    return Handlebars.compile($('#template-' + name).html());
+  }
+
+  var upTo = [{ back: openRoot, name: 'Root'}];
+  var folders,
+      apps,
+      organizations;
+
+  function getApps() {
+    return Fliplet.Apps
+      .get()
+      .then(function (apps) {
+        return apps.filter(function (app) {
+          return !app.legacy;
+        })
+      });
+  }
+
+  function openRoot() {
+    // Clean library container
+    $imagesContainer.html('');
+
+    var organizationId = Fliplet.Env.get('organizationId');
+    return Promise.all([
+      Fliplet.Organizations.get(),
+      getApps()
+    ])
+      .then(function renderRoot(values) {
+        organizations = values[0];
+        apps = values[1];
+
+        values[0].forEach(addOrganization);
+        values[1].forEach(addApp)
+      })
+  }
+
+  function openFolder(folderId) {
+    Fliplet.Media.Folders.get({ type: 'folders', folderId: folderId })
+      .then(renderFolderContent);
+  }
+
+  function openApp(appId) {
+    Fliplet.Media.Folders.get({ type: 'folders', appId: appId })
+      .then(renderFolderContent);
+  }
+  
+  function openOrganization(organizationId) {
+    Fliplet.Media.Folders.get({ type: 'folders', organizationId: organizationId })
+      .then(renderFolderContent);
+  }
+
+  function renderFolderContent(values) {
+    $('.folder-selection').html('Select an folder below')
+    $imagesContainer.html('');
+
+    if (!values.folders.length) {
+      return noFiles();
+    }
+
+    folders = values.folders;
+
+    // Render folders and files
+    _.sortBy(values.folders, ['name']).forEach(addFolder);
+  }
+
+  $('.image-library')
+    .on('dblclick', '[data-folder-id]', function () {
+      var $el = $(this);
+      var id = $el.data('folder-id');
+      var backItem;
+
+      // Store to nav stack
+      backItem = _.find(folders, ['id', id]);
+      backItem.back = function () {
+        openFolder(id);
+      };
+      upTo.push(backItem);
+
+      // Open
+      openFolder(id);
+
+      // Update paths
+      updatePaths();
+    })
+    .on('dblclick', '[data-app-id]', function () {
+      var $el = $(this);
+      var id = $el.data('app-id');
+      var backItem;
+
+      // Store to nav stack
+      backItem = _.find(apps, ['id', id]);
+      backItem.back = function () {
+        openApp(id);
+      };
+      upTo.push(backItem);
+
+      // Open
+      openApp(id);
+
+      // Update paths
+      updatePaths();
+    })
+    .on('dblclick', '[data-organization-id]', function () {
+      var $el = $(this);
+      var id = $el.data('organization-id');
+      var backItem;
+
+      // Store to nav stack
+      backItem = _.find(organizations, ['id', id]);
+      backItem.back = function () {
+        openOrganization(id);
+      };
+      upTo.push(backItem);
+
+      // Open
+      openOrganization(id);
+
+      // Update paths
+      updatePaths();
+
+    })
+    .on('click', '[data-folder-id]', function () {
+      var $el = $(this);
+      // Removes any selected folder
+      $('.image').not(this).each(function(){
+        $(this).removeClass('selected');
+      });
+
+      // Selects clicked folder or deselects clicked folder
+      $el.toggleClass('selected');
+      $('.folder-selection').html('You have selected a folder');
+      data = { folderId: $el.data('folder-id') };
+    })
+    .on('click', '[data-app-id]', function () {
+      var $el = $(this);
+      // Removes any selected folder
+      $('.image').not(this).each(function(){
+        $(this).removeClass('selected');
+      });
+
+      // Selects clicked folder or deselects clicked folder
+      $el.toggleClass('selected');
+      $('.folder-selection').html('You have selected an app folder');
+      data = { appId: $el.data('app-id') };
+    })
+    .on('click', '[data-organization-id]', function () {
+      var $el = $(this);
+      // Removes any selected folder
+      $('.image').not(this).each(function(){
+        $(this).removeClass('selected');
+      });
+
+      // Selects clicked folder or deselects clicked folder
+      $el.toggleClass('selected');
+      $('.folder-selection').html('You have selected an organization folder');
+      data = { organizationId: $el.data('organization-id') };
+    });
+
+  $('.back-btn').click(function () {
+    if (upTo.length === 1) {
+      return;
+    }
+
+    upTo.pop();
+    upTo[upTo.length-1].back();
+    updatePaths();
   });
-}
 
-// Adds app root folder template
-function addApp(app) {
-  $folderList.append(templates.app(app));
-}
+  function updatePaths() {
+    if (upTo.length === 1) {
+      // Hide them
+      $('.back-btn').hide();
+      $('.breadcrumbs-select').hide();
 
-// Adds folder item template
-function addFolder(folder) {
-  $folderList.append(templates.folder(folder));
-}
+      return;
+    }
 
-// Adds organisation item template
-function addOrganisationFolder(org) {
-  $folderList.append(templates.organisation(org));
-}
+    // Show them
+    $('.breadcrumbs-select').show();
+    $('.back-btn').show();
 
-// Templating
-function template(name) {
-  return Handlebars.compile($('#template-' + name).html());
-}
+    // Parent folder
+    $('.up-to').html(upTo[upTo.length - 2].name);
 
-function save() {
-  var $el = $folderList.find('option:selected');
-  var val = $el.val();
-
-  data = {};
-
-  if ($el.data('app')) {
-    data.appId = val;
-  } else if ($el.data('organisation')) {
-    data.organizationId = val;
-  } else {
-    data.folderId = val;
+    // Current folder
+    $('.helper').html(upTo[upTo.length - 1].name);
   }
 
-  Fliplet.Widget.save(data).then(function () {
-    Fliplet.Studio.emit('reload-page-preview');
-    Fliplet.Widget.complete();
+  // init
+  openRoot();
+
+  Fliplet.Widget.onSaveRequest(function () {
+    Fliplet.Widget.save(data).then(function () {
+      Fliplet.Widget.complete();
+    });
   });
-}
-
-Fliplet.Widget.onSaveRequest(function () {
-  save();
-});
-
-$folderList.on('change', function() {
-  var selectedValue = $(this).val();
-  var selectedText = $(this).find("option:selected").text();
-  $('.section.show').removeClass('show');
-  $('#' + selectedValue + 'Section').addClass('show');
-  $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
-});
-
-$('#help_tip').on('click', function() {
-  alert("During beta, please use live chat and let us know what you need help with.");
-});
-
-function initialiseData() {
-  if (!data) {
-    return;
-  }
-
-  var $context;
-
-  if (data.appId) {
-    $context = $folderList.find('[data-app][value="'+data.appId+'"]');
-  } else if (data.organizationId) {
-    $context = $folderList.find('[data-organisation][value="'+data.organizationId+'"]');
-  } else if (data.folderId) {
-    $context = $folderList.find('[data-folder][value="'+data.folderId+'"]');
-  }
-
-  if ($context) {
-    $context.attr("selected","selected");
-  }
-
-  $folderList.change();
-}
-
-initialise();
-
 })();
