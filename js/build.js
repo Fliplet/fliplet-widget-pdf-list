79cf4f5f9f58ee51e08c793587664f44723dd119
Nicholas Valbusa <squallstar@gmail.com>
2016-11-03 22:28:20 +0100

    more tweaks

--- js/build.js
+++ js/build.js
@@ -18,6 +18,10 @@ $('[data-pdf-list-id]').each(function () {
     $el.find('.search-wrapper').attr('data-mode', 'loading');
     $el.find('.search-screen').addClass('loading');
 
+    $el.find('.offline-notification').removeClass('show');
+    $el.find('.offline-screen').removeClass('show');
+    $el.find('.list').removeClass('hidden');
+
     currentFiles = [];
     $listHolder.html('');
 
@@ -34,9 +38,7 @@ $('[data-pdf-list-id]').each(function () {
       $el.find('.search-wrapper').attr('data-mode', 'default');
       $el.find('.search-screen').removeClass('loading');
       $el.find('.list').attr('data-view', 'default');
-      if ( !$el.find('.first-load').hasClass('hidden') ) {
-        $el.find('.first-load').addClass('hidden');
-      }
+      $el.find('.first-load').addClass('hidden');
 
       return Promise.resolve();
     });
@@ -57,20 +59,20 @@ $('[data-pdf-list-id]').each(function () {
   }
 
   Fliplet.Navigator.onReady().then(function () {
-    if ( $el.find('.offline-notification').hasClass('show') ) {
-      $el.find('.offline-notification').removeClass('show');
-      $el.find('.offline-screen').removeClass('show');
-      $el.find('.list').removeClass('hidden');
-    }
-
     getFolderContents().then(function () {
       // success
+      $el.find('.first-load').addClass('hidden');
     }, function () {
       // fail. perhaps device is offline?
       $el.find('.offline-notification').addClass('show');
       $el.find('.offline-screen').addClass('show');
       $el.find('.list').addClass('hidden');
       $el.find('.first-load').addClass('hidden');
+
+      // load files when the device goes back online
+      Fliplet.Navigator.onOnline(function () {
+        getFolderContents();
+      })
     })
   });
 
